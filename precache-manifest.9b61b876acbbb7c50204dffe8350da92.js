8464024019c786c173d0562076b9998ac6d7d10d
rsreecharitha <rsreecharitha@gmail.com>
2020-10-12 14:47:09 +0530

    Updates

---
+++ precache-manifest.9b61b876acbbb7c50204dffe8350da92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57afd4903928a69b3f35",
+    "url": "/visualcrypt/static/css/main.9b65892f.chunk.css"
+  },
+  {
+    "revision": "57afd4903928a69b3f35",
+    "url": "/visualcrypt/static/js/main.57afd490.chunk.js"
+  },
+  {
+    "revision": "07f3aeac447f4cdd0264",
+    "url": "/visualcrypt/static/js/1.07f3aeac.chunk.js"
+  },
+  {
+    "revision": "1008b7cc44027ddf97d0",
+    "url": "/visualcrypt/static/js/runtime~main.1008b7cc.js"
+  },
+  {
+    "revision": "6579b8510d8d0d7d46cc79bd0dd4f04c",
+    "url": "/visualcrypt/index.html"
+  }
+];
